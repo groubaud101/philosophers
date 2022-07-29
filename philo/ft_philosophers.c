2e74e18026a4bff66e7e3b69c8af79fdf8db4af3
groubaud101 <guillaume.roubaud1@gmail.com>
2022-07-29 07:24:16 +0200

    on se revoit dans un mois

--- philo/ft_philosophers.c
+++ philo/ft_philosophers.c
@@ -6,7 +6,7 @@
 /*   By: groubaud <groubaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 17:29:35 by groubaud          #+#    #+#             */
-/*   Updated: 2022/07/28 12:54:49 by groubaud         ###   ########.fr       */
+/*   Updated: 2022/07/28 14:13:17 by groubaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(int ac, char **av)
 	t_master	master;
 
 	if (ac < 5 || ac > 6)
-		return (ft_print_error("Wrong number of argument"));
+		return (ft_print_error("Wrong number of argument\n"));
 	if (ft_init_master(ac, av, &master))
 		return (1);
 
